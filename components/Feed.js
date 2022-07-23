0dad6c651c2908e8d688f19205e62c2bf138c8e4
manishchaurasiy <109471315+manishchaurasiy@users.noreply.github.com>
2022-07-23 14:01:04 +0530

    add the post section of the feed component

--- components/Feed.js
+++ components/Feed.js
@@ -1,7 +1,28 @@
 import { SparklesIcon } from "@heroicons/react/outline";
 import Input from "./Input";
+import Post from "./Post";
 
 export default function Feed() {
+  const posts = [
+    {
+      id: "1",
+      name: "Manish Chaurasia",
+      username: "manish56",
+      userImg: "https://media.wired.com/photos/5fb70f2ce7b75db783b7012c/master/pass/Gear-Photos-597589287.jpg",
+      img: "https://images.unsplash.com/photo-1658191342762-989ac7137464?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      text: "nice picture!",
+      timestamp: "3 hours ago"
+    },
+    {
+      id: "2",
+      name: "Alisha",
+      username: "alisha78",
+      userImg: "https://images.unsplash.com/photo-1658104917506-67c6e95f1dfe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDMxfHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      img: "https://images.unsplash.com/photo-1644982647531-daff2c7383f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+      text: "Fantastic!",
+      timestamp: "24 minutes ago"
+    },
+  ]
   return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
         <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
@@ -11,7 +32,9 @@ export default function Feed() {
             </div>
         </div>
         <Input />
-        
-        </div>
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
+      </div>
   );
 }
