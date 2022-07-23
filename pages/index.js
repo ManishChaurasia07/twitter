34a02db76a2cefeab37a7d43969ab44d85a023ab
manishchaurasiy <109471315+manishchaurasiy@users.noreply.github.com>
2022-07-23 16:11:28 +0530

    create the random users section of the widgets component

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Feed from '../components/Feed';
 import Sidebar from '../components/Sidebar';
 import Widgets from '../components/Widgets';
 
-export default function Home({newsResults}) {
+export default function Home({newsResults, randomUsersResults}) {
   return (
     <div>
       <Head>
@@ -20,7 +20,7 @@ export default function Home({newsResults}) {
 
 
     {/*Widgets*/}
-    <Widgets newsResults={newsResults.articles} />
+    <Widgets newsResults={newsResults.articles} randomUsersResults={randomUsersResults.results}/>
 
     {/*Modal*/}
 
@@ -40,9 +40,14 @@ export default function Home({newsResults}) {
 
 export async function getServerSideProps(){
   const newsResults = await fetch ("https://saurav.tech/NewsAPI/top-headlines/category/business/in.json").then((res) => res.json());
-  return{
+  
+  //who to follow section//
+  const randomUsersResults = await fetch("https://randomuser.me/api/?results=30&inc=name,login,picture").then((res)=>res.json());
+
+  return {
     props:{
       newsResults,
-    }
-  }
+      randomUsersResults,
+    },
+  };
 }
