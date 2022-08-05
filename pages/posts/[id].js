06db9c846165dd0e032c90cf9fefdaa310842ec2
manishchaurasiy <109471315+manishchaurasiy@users.noreply.github.com>
2022-08-05 22:02:33 +0530

    get the comments from firestore and create a comment post  page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -7,16 +7,27 @@ import Post from '../../components/Post';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { db } from '../../firebase';
-import { doc, onSnapshot } from 'firebase/firestore';
+import { collection, doc, onSnapshot, orderBy, query } from 'firebase/firestore';
+import Comment from '../../components/comment';
 
 
 export default function PostPage({newsResults, randomUsersResults}) {
   const router = useRouter();
   const {id} = router.query;
-  const [post, setPost] =useState();
+  const [post, setPost] = useState();
+  const [comments, setComments] = useState([]);
 
+  //get post data//
 
   useEffect(() => onSnapshot(doc(db, "posts", id), (snapshot) => setPost(snapshot)), [db, id]);
+
+  // get comments from the post//
+
+  useEffect(() => {onSnapshot(query(collection(db, "posts", id, "comments" ), orderBy("timestamp", "desc")
+  ), (snapshot) => setComments(snapshot.docs)
+  ); 
+}, [db, id]);
+
   return (
     <div>
       <Head>
@@ -36,7 +47,15 @@ export default function PostPage({newsResults, randomUsersResults}) {
             </div>
             <h2 className="text-lg sm:text-xl font-bold cursor-pointer">Tweet</h2>
             </div>
-            <Post id={id} post={post}/>
+            <Post id={id} post={post} />
+            {comments.length > 0 && (
+              <div className="">
+                {comments.map((comment) => (
+                <Comment key={comment.id} id={comment.id} comment={comment.data()}/>
+              ))}
+              </div>
+            )}
+   
       </div>
    
   {/*Widgets*/}
