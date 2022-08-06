9f47c1e23815eb9706d80d6a2cc626152da7cd25
manishchaurasiy <109471315+manishchaurasiy@users.noreply.github.com>
2022-08-06 16:48:09 +0530

    fix deployment errors

--- components/News.js
+++ components/News.js
@@ -1,7 +1,8 @@
 
 
 export default function News({article}) {
-  return <a href={article.url} target="_blank">
+  return (
+  <a rel="noreferrer" href={article.url} target="_blank">
     <div className="flex items-center justify-between px-4 py-2 space-x-1 hover:bg-gray-200 transition duration-500 ease-out">
         <div className="space-y-0.5 ">
             <h6 className="text-sm font-bold">{article.title}</h6>
@@ -9,6 +10,5 @@ export default function News({article}) {
         </div>
         <img className="rounded-xl " width="70" src={article.urlToImage} alt=""></img>
     </div>
-
-  </a>;
-}
+  </a>
+)};
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -9,6 +9,7 @@ import { useEffect, useState } from 'react';
 import { db } from '../../firebase';
 import { collection, doc, onSnapshot, orderBy, query } from 'firebase/firestore';
 import Comment from '../../components/comment';
+import { AnimatePresence, motion } from 'framer-motion';
 
 
 export default function PostPage({newsResults, randomUsersResults}) {
@@ -50,9 +51,13 @@ export default function PostPage({newsResults, randomUsersResults}) {
             <Post id={id} post={post} />
             {comments.length > 0 && (
               <div className="">
+                 <AnimatePresence>
                 {comments.map((comment) => (
+                  <motion.div key={comment.id} initial={{opacity: 0}} animate={{opacity:1}} exit={{opacity: 0}} transition={{duration: 1}} >
                 <Comment key={comment.id} commentId={comment.id} originalPostId={id} comment={comment.data()}/>
+                </motion.div>
               ))}
+              </AnimatePresence>
               </div>
             )}
    
